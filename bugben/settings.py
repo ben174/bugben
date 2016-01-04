565c46388659c79c7cdbbe687cee2c01e3aaf4ed
Ben Friedland <ben174@gmail.com>
2016-01-03 18:16:13 -0800

    Production ready

--- bugben/settings.py
+++ bugben/settings.py
@@ -82,6 +82,21 @@ DATABASES = {
     }
 }
 
+if 'DB_NAME' in os.environ.keys():
+    # running in production
+    print '********** PRODUCTION *************'
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASS'],
+            'HOST': os.environ['DB_SERVICE'],
+            'PORT': os.environ['DB_PORT']
+        }
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
