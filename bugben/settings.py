eb90c7fdfc41edade7b03c678a582fcc7354269f
Ben Friedland <ben174@gmail.com>
2020-06-04 21:24:36 -0700

    move static back

--- bugben/settings.py
+++ bugben/settings.py
@@ -122,11 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "static"),
-os.path.join(BASE_DIR, "resume", "static"),
-                ]
-
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
